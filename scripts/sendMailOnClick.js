adf0bdf9551ef4bc331ae9f41673c88bfa2f7653
nyzra <sauronlucas@gmail.com>
2023-04-27 16:22:30 +0200

    déplacement du code pour la fonction sendmail dans un fichier js dans scripts/

--- scripts/sendMailOnClick.js
+++ scripts/sendMailOnClick.js
@@ -1,1 +1,13 @@
+sendMailButton = $(".sendMailButton")
 
+function SendMail() {
+    let filter = window.filter
+    let fiches = filter.filteredFiches
+    console.log(fiches)
+  //window.location.href =
+    "mailto:user@example.com?subject=Fiches Non Conformes&body=Bonjour,%0DSuite a une vérification des fiches métier nous avons pu voir que certaines des fiches dont vous etes le référent ne sont pas conforme.%0DVoici une liste exhaustive des fiches que vous devez remettre en règle dans les plus bref délai :%0D";
+}
+
+mailButton.click(function(){
+    SendMail()
+});
